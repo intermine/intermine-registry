fcfe71f56f5e54c40af51031a94d81f336399b72
lkuffo <lkuffo@espol.edu.ec>
2017-05-29 23:34:02 -0500

    API Endpoint: Get One Instance + All Instances Query

--- registryapp/routes/instances.js
+++ registryapp/routes/instances.js
@@ -10,7 +10,40 @@ router.get('/', function(req, res, next) {
         if (err){
             res.send(err);
         }
-        res.json(instances);
+        var api_response = {};
+        if (req.query.q){
+            var filtered_instances = [];
+            var query = req.query.q.toLowerCase();
+            for (var i in instances){
+                var instance = instances[i];
+                if (instance['description'].toLowerCase().indexOf(query) !== -1 || instance['organisms'].includes(query) || instance['name'].toLowerCase().indexOf(query) !== -1){
+                    filtered_instances.push(instance);
+                }
+            }
+            api_response['instances'] = filtered_instances
+        }else{
+            api_response['instances'] = instances;
+        }
+        api_response['statusCode'] = 200;
+        api_response['executionTime'] = new Date().toLocaleString();
+        res.json(api_response);
+    });
+});
+
+router.get('/:id', function(req, res, next) {
+    var toFind = req.params.id;
+    Instance.find({
+        id: toFind
+    },
+    function(err, instances){
+        if (err){
+            res.send(err);
+        }
+        api_response = {};
+        api_response['instance'] = instances[0];
+        api_response['statusCode'] = 200;
+        api_response['executionTime'] = new Date().toLocaleString();
+        res.json(api_response);
     });
 });
 
