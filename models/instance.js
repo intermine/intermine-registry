839c7cb74990617c9052859f27b42c3091a132f0
danielabutano <daniela@intermine.org>
2017-07-24 11:35:01 +0100

    added env variable for MongoDB

--- models/instance.js
+++ models/instance.js
@@ -4,12 +4,8 @@
  */
 var mongoose = require('mongoose');
 
-var mongoDB = 'mongodb://lkuffo2:sandbox1@ds115712.mlab.com:15712/intermineregistry';
-var DB_USER = process.env.DB_USER;
-var DB_PASS = process.env.DB_PASS;
-//var mongoDB = 'mongodb://'+ DB_USER + ':' + DB_PASS + '@ds115712.mlab.com:15712/intermineregistry';
-
-mongoose.connect(mongoDB);
+//var mongoDB = 'mongodb://lkuffo2:sandbox1@ds115712.mlab.com:15712/intermineregistry'
+mongoose.connect(process.env.MONGODB_URL);
 
 var db = mongoose.connection;
 
