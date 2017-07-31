f0cf38700b23b33d555cd9085eb1080c87b797dd
lkuffo <lkuffo@espol.edu.ec>
2017-07-31 16:41:17 -0500

    Passport-local Logic Configuration

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require("passport");
+var Strategy = require('passport-local').Strategy;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var passport = require('passport');
 
 router.get('/login', function(req, res, next){
     res.render('login', { user: req.user });
