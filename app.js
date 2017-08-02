bd3b96dcfcfeb45355bf1459e79532768f3c6f4f
lkuffo <lkuffo@espol.edu.ec>
2017-08-02 04:45:00 -0500

    Modularized Authentication

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require("passport");
-var Strategy = require('passport-local').Strategy;
-var BasicStrategy = require('passport-http').BasicStrategy;
+
 
 var index = require('./routes/index');
 var instances = require('./routes/instances');
@@ -19,65 +18,7 @@ const scheduledAutomaticUpdate = require('./scheduled/automaticUpdate');
 
 var User = require('./models/user');
 
-
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-
-passport.use(new Strategy(
-  function(username, password, done) {
-    User.findOne({ 'user': username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect Username.' });
-      }
-      if (user.password != password) {
-        return done(null, false, { message: 'Incorrect Password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-
-
-passport.use(new BasicStrategy(
-  function(username, password, done) {
-    console.log(username);
-    console.log(password);
-    User.findOne({ 'user': username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect Username.' });
-      }
-      if (user.password != password) {
-        return done(null, false, { message: 'Incorrect Password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user._id);
-});
-
-passport.deserializeUser(function(id, cb) {
-	User.findOne({ '_id': id }, function(err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
+var auth = require('./routes/auth');
 
 var app = express();
 
