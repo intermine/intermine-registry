1d5e57330a08dd5e18da89db66c691e9c33b9f13
lkuffo <lkuffo@espol.edu.ec>
2017-08-25 05:25:16 -0500

    DB for production connection

--- models/instance.js
+++ models/instance.js
@@ -4,8 +4,8 @@
 // Registry Connection
 var mongoose = require('mongoose');
 
-var mongoDB = 'mongodb://lkuffo2:sandbox1@ds115712.mlab.com:15712/intermineregistry'
-mongoose.connect(mongoDB);
+//var mongoDB = 'mongodb://lkuffo2:sandbox1@ds115712.mlab.com:15712/intermineregistry'
+mongoose.connect(process.env.MONGODB_URL);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
