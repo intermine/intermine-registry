f103cc77694a8ad57a5131f2932b67cb9af5e33f
Daniela Butano <daniela@intermine.org>
2018-06-30 15:43:01 -0700

    added 2 new endpoints for galaxy

--- routes/index.js
+++ routes/index.js
@@ -231,4 +231,34 @@ router.post('/instance', function(req, res, next) {
     });
 });
 
+/**
+ * Endpoint:  /
+ * Method:    GET
+ * Description: Render home page, sending user as parameter.
+ */
+router.get('/im-to-galaxy', function(req, res, next) {
+    if (typeof (req.query.success)){
+      var operation = req.query.success;
+      if (operation == 1){
+        return res.render('index', { user: req.user, message: "Instance Added Successfully" });
+      } else if (operation == 2) {
+        return res.render('index', { user: req.user, message: "Instance Updated Successfuly" });
+      }
+    }
+    return res.render('index', { user: req.user });
+});
+
+router.get('/galaxy-to-im', function(req, res, next) {
+    if (typeof (req.query.success)){
+      var operation = req.query.success;
+      if (operation == 1){
+        return res.render('index', { user: req.user, message: "Instance Added Successfully" });
+      } else if (operation == 2) {
+        return res.render('index', { user: req.user, message: "Instance Updated Successfuly" });
+      }
+    }
+    return res.render('index', { user: req.user });
+});
+
+
 module.exports = router;
