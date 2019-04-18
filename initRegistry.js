4b80b3844bee43266c6318df02bd740db3391032
Yo Yehudi <yo@intermine.org>
2019-04-18 17:25:38 +0100

    add sparse setup insrtuctions

--- initRegistry.js
+++ initRegistry.js
@@ -1,57 +1,64 @@
 /**
-  * File used to init a raw copy of InterMine Registry on a new DB environment.
-  *
-  */
+ * File used to init a raw copy of InterMine Registry on a new DB environment.
+ *
+ */
 
 var request = require('request');
 var asyncLoop = require('node-async-loop');
 
 mines = [
-    ["FlyMine", 'http://www.flymine.org/query', "flymine"],
-    ["HumanMine", 'http://www.humanmine.org/humanmine', "humanmine"],
-    ["MouseMine", 'http://www.mousemine.org/mousemine', "mousemine"],
-    ["YeastMine", 'https://yeastmine.yeastgenome.org/yeastmine', "yeastmine"],
-    ["ZebrafishMine", 'http://www.zebrafishmine.org/', "zebrafishmine"],
-    ["WormMine", 'http://intermine.wormbase.org/tools/wormmine',"wormmine"],
-    ["SoyMine", 'https://mines.legumeinfo.org/soymine',"soymine"],
-    ["BeanMine", 'https://mines.legumeinfo.org/beanmine', "beanmine"],
-    ["MedicMine", 'http://medicmine.jcvi.org/medicmine',"medicmine"],
-    ["PeanutMine", 'https://mines.legumeinfo.org/peanutmine',"peanutmine"],
-    ["ChickpeaMine", 'https://mines.legumeinfo.org/chickpeamine', "chickpeamine"],
-    ["LegumeMine", 'https://intermine.legumefederation.org/legumemine', "legumemine"],
-    ["RatMine", 'http://ratmine.mcw.edu/ratmine',"ratmine"],
-    ["WheatMine", 'http://urgi.versailles.inra.fr/WheatMine', "wheatmine"],
-    ["RepetDB", 'http://urgi.versailles.inra.fr/repetdb',"repetdb"],
-    ["ThaleMine", 'https://apps.araport.org/thalemine',"thalemine"],
-    ["BovineMine", 'http://128.206.116.12:8080/bovinemine/begin.do',"bovinemine"],
-    ["PhytoMine", 'https://phytozome.jgi.doe.gov/phytomine',"phytomine"],
-    ["HymenopteraMine", 'http://hymenopteragenome.org/hymenopteramine', "hymenopteramine"],
-    ["Mitominer", 'http://mitominer.mrc-mbu.cam.ac.uk/release-4.0', "mitominer"],
-    ["TargetMine", 'http://targetmine.mizuguchilab.org/targetmine', "targetmine"],
-    ["INDIGO", "http://www.cbrc.kaust.edu.sa/indigo", "indigo"],
-    ["Shaare", "http://www.shaare.org.uk/release-1.0","shaare"],
-    ["XenMine", "http://www.xenmine.org/xenmine","xenmine"],
-    ["GrapeMine", "http://urgi.versailles.inra.fr/GrapeMine","grapemine"],
-    ["PlanMine", "http://planmine.mpi-cbg.de/planmine","planmine"],
-    ["TetraMine", "http://adenine.bradley.edu/tetramine","tetramine"],
-    ["modMine", "http://intermine.modencode.org/release-33","modmine"],
-    ["CHOmine", "https://chomine.boku.ac.at/chomine","chomine",]
+  ["FlyMine", 'http://www.flymine.org/query', "flymine"],
+  ["HumanMine", 'http://www.humanmine.org/humanmine', "humanmine"],
+  ["MouseMine", 'http://www.mousemine.org/mousemine', "mousemine"],
+  ["YeastMine", 'https://yeastmine.yeastgenome.org/yeastmine', "yeastmine"],
+  ["ZebrafishMine", 'http://www.zebrafishmine.org/', "zebrafishmine"],
+  ["WormMine", 'http://intermine.wormbase.org/tools/wormmine', "wormmine"],
+  ["SoyMine", 'https://mines.legumeinfo.org/soymine', "soymine"],
+  ["BeanMine", 'https://mines.legumeinfo.org/beanmine', "beanmine"],
+  ["MedicMine", 'http://medicmine.jcvi.org/medicmine', "medicmine"],
+  ["PeanutMine", 'https://mines.legumeinfo.org/peanutmine', "peanutmine"],
+  ["ChickpeaMine", 'https://mines.legumeinfo.org/chickpeamine', "chickpeamine"],
+  ["LegumeMine", 'https://intermine.legumefederation.org/legumemine', "legumemine"],
+  ["RatMine", 'http://ratmine.mcw.edu/ratmine', "ratmine"],
+  ["WheatMine", 'http://urgi.versailles.inra.fr/WheatMine', "wheatmine"],
+  ["RepetDB", 'http://urgi.versailles.inra.fr/repetdb', "repetdb"],
+  ["ThaleMine", 'https://apps.araport.org/thalemine', "thalemine"],
+  ["BovineMine", 'http://128.206.116.12:8080/bovinemine/begin.do', "bovinemine"],
+  ["PhytoMine", 'https://phytozome.jgi.doe.gov/phytomine', "phytomine"],
+  ["HymenopteraMine", 'http://hymenopteragenome.org/hymenopteramine', "hymenopteramine"],
+  ["Mitominer", 'http://mitominer.mrc-mbu.cam.ac.uk/release-4.0', "mitominer"],
+  ["TargetMine", 'http://targetmine.mizuguchilab.org/targetmine', "targetmine"],
+  ["INDIGO", "http://www.cbrc.kaust.edu.sa/indigo", "indigo"],
+  ["Shaare", "http://www.shaare.org.uk/release-1.0", "shaare"],
+  ["XenMine", "http://www.xenmine.org/xenmine", "xenmine"],
+  ["GrapeMine", "http://urgi.versailles.inra.fr/GrapeMine", "grapemine"],
+  ["PlanMine", "http://planmine.mpi-cbg.de/planmine", "planmine"],
+  ["TetraMine", "http://adenine.bradley.edu/tetramine", "tetramine"],
+  ["modMine", "http://intermine.modencode.org/release-33", "modmine"],
+  ["CHOmine", "https://chomine.boku.ac.at/chomine", "chomine", ]
 ]
 
-var host = '';  // Host where InterMine registry is running
+var host = 'http://localhost:3000'; // Host where InterMine registry is running
 var username = '';
 var password = '';
 
-asyncLoop(mines, function(mine, next){
-    var mineName = mine[0];
-    var mineURL = mine[1];
-    var mineNamespace = mine[2];
-    var req = {
+//check host is set correctly & error if not.
+
+request.get(host, function(err, res, body) {
+
+  if (err) {
+    console.error("No registry server found at ", host, " check the server is running and the url is set correctly");
+  } else {
+    asyncLoop(mines, function(mine, next) {
+      var mineName = mine[0];
+      var mineURL = mine[1];
+      var mineNamespace = mine[2];
+      var req = {
         "name": mineName,
         "url": mineURL,
-        "namespace" : namespace
-    };
-    request.post({
+        "namespace": mineNamespace
+      };
+      request.post({
         json: true,
         url: host + "/service/instances",
         body: req,
@@ -59,9 +66,14 @@ asyncLoop(mines, function(mine, next){
           "user": username,
           "pass": password
         },
-    }, function(err, res, body){
-        console.log(mineName + " ==> " + body.friendlyMessage);
-        next();
+      }, function(err, res, body) {
+        if (!err) {
+          console.log(mineName + " ==> " + body.friendlyMessage);
+          next();
+        } else {
+          console.error(err)
+        }
+      });
     });
-})
-
+  }
+});
