8c3e4fd8642d83179e9fb3d367b089339ab12d66
akshat <akshatbhargavadeepa@gmail.com>
2019-04-18 18:21:44 +0530

    refactored code - using const instead of var wherever possible, handling user already exists state and showing colored error and success messages in createUser.

--- createUser.js
+++ createUser.js
@@ -1,9 +1,9 @@
-var User = require('./models/user');
-var mongoose = require('mongoose');
+const User = require('./models/user');
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URL);
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const readline = require('readline');
@@ -13,19 +13,36 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Please enter the username : ', (username) => {
-	rl.question('Please enter the password : ', (password) => {
-        var newUser = User({
-        	user : username,
-        	password : password
-        });
-        
-        User.createUser(newUser, function(error){
-        	if(error)
-        		throw error;
-        	else
-        		console.log("User with username " + newUser.user + " created..");
-        	rl.close(process.exit(1));
-        });
-    });
-});
+const print_error = '\x1b[31m%s\x1b[0m'; // prints string injected in red
+const print_success = '\x1b[32m%s\x1b[0m'; // prints string injected in green
+
+function getInfoAndCreate() {
+  rl.question('Please enter the username : ', (username) => {
+    rl.question('Please enter the password : ', (password) => {
+          const newUser = User({
+            user : username,
+            password : password
+          });
+
+          User.createUser(newUser, function(error){
+            if (error) {
+              switch (error.code) {
+                case 11000: {
+                  console.log(print_error, 'User with provided name already exists! Try again with different name!');
+                  getInfoAndCreate();
+                  return;
+                }
+                default: {
+                  throw error;
+                }
+              }
+            } else {
+              console.log(print_success, "User with username " + newUser.user + " created.");
+              rl.close(process.exit(1));
+            }
+          });
+      });
+  });
+}
+
+getInfoAndCreate();
